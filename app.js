dfa872f54d6ae54f704fe6e623f379d268db750d
eleciii4estvo <gulevsky_iv@mail.ru>
2023-11-11 00:09:53 +0300

    add new popup window and new universal js function

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-//открывем модальное окно при клике на кнопку
-document.querySelector("#info_about_content_btn").addEventListener("click", function(){
-    document.querySelector("#modal_about_content").classList.add("open")
-})
-
-//закрываем модальное окно при клике на крестик
-document.querySelector("#close_modal_btn").addEventListener("click", function(){
-    document.querySelector("#modal_about_content").classList.remove("open")
-})
-
-//закрываем модальное окно при клике на темную область экрана
-document.querySelector("#modal_about_content .modal_box").addEventListener("click", e=>{
-    e._isClickInModal=true;
-})
-document.querySelector("#modal_about_content").addEventListener("click", e=>{
-    if(e._isClickInModal) return;
-    e.currentTarget.classList.remove("open");
-})
-
-
-// addEventListener("click", event => {
-//     if (event.target.id) {
-//         console.log("ID элемента:", event.target.id);
-//     } else {
-//         console.log("Элемент не имеет атрибута id");
-//     }
-// });
+let currentOpenModal = null;
+addEventListener("click", event => {
+    if (event.target.id) {
+        const targetId = event.target.id;
+        const targetPopup = document.querySelector(`#${targetId}_popup`);
+        if (targetId) {
+            if (targetId !== "close_modal_img") {
+                // Открываем модальное окно
+                targetPopup.classList.add("open");
+                // Запоминаем текущее открытое модальное окно
+                currentOpenModal = targetPopup;
+            } else {
+                // Закрываем текущее открытое модальное окно
+                if (currentOpenModal) {
+                    currentOpenModal.classList.remove("open");
+                    // Сбрасываем текущее открытое модальное окно
+                    currentOpenModal = null;
+                }
+            }
+        }
+    } else {
+        console.log("Элемент не имеет атрибута id");
+    }
+});
